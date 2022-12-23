20939b61a5dbb296b83e33d4ad4a0de8a17ed762
Amit Chhabra <chhabra@Amits-Mac-mini.local>
2022-12-23 11:45:53 -0800

    First version of viewNoViews

---
+++ background.js
@@ -0,0 +1,38 @@
+
+function removeViews(){
+   linksa = document.querySelectorAll('a[aria-label*="View Tweet analytics"]');
+
+
+for (let element of linksa) {
+    if (element == undefined){
+        continue
+    }
+  element.remove();
+}}
+
+
+
+function debounce(func, wait) {
+  let timeout;
+  return function() {
+    clearTimeout(timeout);
+    timeout = setTimeout(func, wait);
+  }
+}
+
+const debouncedDeleteElements = debounce(removeViews, 120);
+
+window.onscroll = function() {
+  debouncedDeleteElements();
+}
+
+// Delete the elements after the ad blocker has run
+var observer = new MutationObserver(function() {
+  removeViews()
+});
+
+observer.observe(document.body, {
+  childList: true,
+  subtree: true
+});
+
